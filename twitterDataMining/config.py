64727d5ee5f491c25670128e852cf843dcbd11d5
CodeBear <32265264+CodeBear02@users.noreply.github.com>
2019-08-23 16:06:29 +0100

    Create config.py

---
+++ twitterDataMining/config.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Aug 23 16:00:27 2019
+
+@author: ryan.nanson
+"""
+
+consumer_key = 'your-consumer-key'
+consumer_secret = 'your-consumer-secret'
+access_token = 'your-access-token'
+access_secret = 'your-access-secret'
